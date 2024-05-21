fb3a2ff0e1abf538859b404c8f818e9618f2dfda
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-21 19:04:58 +0100

    Create A2.js

---
+++ plugins/A2.js
@@ -0,0 +1,33 @@
+let guessImpostor = async (m, { conn, command }) => {
+    let id = m.chat;
+    if (!gameActive) {
+        conn.reply(m.chat, 'لم تبدأ أي لعبة. أرسل ".امبوستر" لبدء لعبة جديدة.');
+        return;
+    }
+
+    if (players.length < 3) {
+        conn.reply(m.chat, 'اللعبة تحتاج إلى 3 لاعبين على الأقل.');
+        return;
+    }
+
+    const guess = parseInt(command.split(' ')[1], 10);
+    if (isNaN(guess) || guess < 1 || guess > players.length) {
+        conn.reply(m.chat, 'رقم غير صالح.');
+        return;
+    }
+
+    if (guess - 1 === impostorIndex) {
+        conn.reply(m.chat, `لقد وجدت المحتال! هو اللاعب رقم ${guess}.`);
+        gameActive = false;
+        players = [];
+        impostorIndex = -1;
+    } else {
+        conn.reply(m.chat, 'هذا ليس المحتال! حاول مرة أخرى.');
+    }
+}
+
+guessImpostor.help = ['خمن <رقم>']
+guessImpostor.tags = ['among']
+guessImpostor.command = /^خمن \d+$/i
+
+export default guessImpostor;
