d89bf1b0ecf55871815763c4b27de861d8b5bdc2
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 21:39:27 +0100

    Update and rename wwleft.js to مستذئب_خروج.js

--- plugins/مستذئب_خروج.js
+++ plugins/مستذئب_خروج.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
   let werewolf = global.db.data.werewolf || {}
   if (typeof werewolf.status == "undefined" || werewolf.status != "playing") {
-    conn.reply(m.chat, "Werewolf game is not in progress", m)
+    conn.reply(m.chat, "لعبة المستذئبين ليست قيد التقدم🐺", m)
     return
   }
   if (typeof werewolf.players[m.sender] == "undefined") {
-    conn.reply(m.chat, "You are not a participant in this game", m)
+    conn.reply(m.chat, "أنت لست مشاركا في هذه اللعبة❌", m)
     return
   }
   delete werewolf.players[m.sender]
@@ -18,11 +18,11 @@ let handler = async (m, { conn }) => {
     werewolf.wolves.splice(index, 1)
   }
   global.db.data.werewolf = werewolf
-  conn.reply(m.chat, "You have left the game", m)
+  conn.reply(m.chat, "لقد تركت اللعبة🚀", m)
 }
 handler.help = ['wwleft']
 handler.tags = ['game']
-handler.command = /^wwleft$/i
+handler.command = /^مستذئب_خروج$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
