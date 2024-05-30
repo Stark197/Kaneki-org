9977e486cc678284b20b32b2f5f83abbf1a10630
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 20:54:07 +0100

    Update and rename anime-ppcp.js to تطقيم\.js

--- plugins/تطقيم\.js
+++ plugins/تطقيم\.js
@@ -2,10 +2,10 @@ import { Couples } from 'dhn-api'
 var handler = async (m, { conn, command }) => {
 
 const res = await Couples() 
-await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'ولد', m) 
-return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'بنت', m)
+await conn.sendFile(m.chat, res.male, 'ppcp.jpg', '🧛ولد\nانه فخ حاول تجنبه', m) 
+return conn.sendFile(m.chat, res.female, 'ppcp.jpg', '🧛‍♀️بنت', m)
 }
 handler.help = ['ppcouple']
-handler.tags = ['internet']
+handler.tags = ['ادوات']
 handler.command = /^(تطقيم|ppcouple)$/i
 export default handler
