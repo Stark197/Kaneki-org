ea9555eaabe481327a1841ef9f9a5089778dbcf4
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 21:44:27 +0100

    Update and rename wwplayer.js to مستذئبين_لاعبين.js

--- plugins/مستذئبين_لاعبين.js
+++ plugins/مستذئبين_لاعبين.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   let werewolf = global.db.data.werewolf || {}
   if (typeof werewolf.players == "undefined" || Object.keys(werewolf.players).length == 0) {
-    conn.reply(m.chat, "There are no players in this game", m)
+    conn.reply(m.chat, "لا يوجد لاعبين في هذه اللعبة", m)
     return
   }
   let players = Object.keys(werewolf.players)
@@ -11,20 +11,20 @@ let handler = async (m, { conn }) => {
   for (let i = 0; i < players.length; i++) {
     let player = players[i]
     let role = werewolf.players[player]
-    if (role == "villager") {
+    if (role == "قروي") {
       villagerCount++
-    } else if (role == "werewolf") { // mengganti wolf menjadi werewolf agar dapat lebih mudah dibaca
+    } else if (role == "مستذئب") { // mengganti wolf menjadi werewolf agar dapat lebih mudah dibaca
       wolfCount++
     }
     let playerName = conn.getName(player) // mengambil nama pengguna dengan menggunakan fungsi getName
     playerList += `${i + 1}. ${playerName} (@${player.split("@")[0]})\n` // mengubah format penomoran dan mengganti tanda ` dengan $
   }
-  conn.reply(m.chat, `There are ${players.length} players in this game, including ${villagerCount} villagers and ${wolfCount} werewolves:\n\n${playerList}`, m) // mengubah format pesan agar lebih mudah dibaca
+  conn.reply(m.chat, `هناك ${players.length} اللاعبين في هذه اللعبة، بما في ذلك ${villagerCount} قرويين و ${wolfCount} مستذئبين:\n\n${playerList}`, m) // mengubah format pesan agar lebih mudah dibaca
 }
 
 handler.help = ['wwplayer']
-handler.tags = ['game']
-handler.command = /^wwplayer$/i
+handler.tags = ['العاب']
+handler.command = /^مستذئبين_لاعبين$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
