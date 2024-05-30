cdeaf1be6d3b3079ab20e47cef80c6f1ac95bd59
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 18:00:27 +0100

    Update anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -2,10 +2,10 @@ import { Couples } from 'dhn-api'
 var handler = async (m, { conn, command }) => {
 
 const res = await Couples() 
-await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Male', m) 
-return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Female', m)
+await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'ولد', m) 
+return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'بنت', m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['internet']
-handler.command = /^(ppcp|ppcouple)$/i
+handler.command = /^(تطقيم|ppcouple)$/i
 export default handler
