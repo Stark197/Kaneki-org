f6dbac135fb20626e6f00f300858345caa772fa2
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-31 00:46:16 +0100

    Update and rename downloader-pinterest.js to التحميلات_بينترست.js

--- plugins/التحميلات_بينترست.js
+++ plugins/التحميلات_بينترست.js
@@ -5,18 +5,18 @@ import { URL_REGEX } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     text = text.endsWith('SMH') ? text.replace('SMH', ''): text
-    if (!text) throw 'Input Pinterest Url'
+    if (!text) throw 'لا تنسى الرابط او اسم البحث يا حب'
     let res = await pinterest(text)
     // if (!res) throw res
     let mime = await lookup(res)
     let link = await shortUrl(res)
     text.match(URL_REGEX) ?
-    await conn.sendMessage(m.chat, { [mime.split('/')[0]] : { url: res }, caption: `Succes Download: ${await shortUrl(res)}` }, { quoted: m }) :
-    await conn.sendFile(m.chat, res, text + '.jpeg', `Result From: ${text.capitalize()}\n${link}`, m, false)
+    await conn.sendMessage(m.chat, { [mime.split('/')[0]] : { url: res }, caption: `تم التحميل: ${await shortUrl(res)}` }, { quoted: m }) :
+    await conn.sendFile(m.chat, res, text + '.jpeg', `النتيجة: ${text.capitalize()}\n${link}`, m, false)
 }
 handler.help = ['pinterest <pencarian>']
-handler.tags = ['downloader', 'search']
-handler.command = /^(downloadpin|downloadpinterest|pindl|dlpin)$/i;
+handler.tags = ['التحميلات']
+handler.command = ['بينترست'];
 
 handler.limit = true
 handler.register = true
@@ -33,13 +33,13 @@ async function pinterest(query) {
         })
         let $ = cheerio.load(await res.text())
         let data = $('table[class="table table-condensed table-striped table-bordered"]').find('a').attr('href')
-        if (!data) throw 'Can\'t download post :/'
+        if (!data) throw 'فشل التحميل'
         return data
     } else {
         let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${query}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${query}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`)
         let json = await res.json()
         let data = json.resource_response.data.results
-        if (!data.length) throw `Query "${query}" not found :/`
+        if (!data.length) throw `الاستعمالات "${query}" لا توجد:/`
         return data[~~(Math.random() * (data.length))].images.orig.url
     }
 }
