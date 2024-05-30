6f52231c5e3db2961160a4087da19e56e1b5c0fd
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 21:35:24 +0100

    Update and rename wwjoin.js to مستذئب_انضم.js

--- plugins/مستذئب_انضم.js
+++ plugins/مستذئب_انضم.js
@@ -1,21 +1,21 @@
 let handler = async (m, { conn }) => {
   let werewolf = global.db.data.werewolf || {}
   if (typeof werewolf.status == "undefined" || werewolf.status != "playing") {
-    conn.reply(m.chat, "Werewolf game is not in progress", m)
+    conn.reply(m.chat, "لعبة المستذئب ليست قيد التقدم", m)
     return
   }
   if (typeof werewolf.players[m.sender] != "undefined") {
-    conn.reply(m.chat, "You have already joined the game", m)
+    conn.reply(m.chat, "لقد انضممت بالفعل الى اللعبة🐺", m)
     return
   }
-  werewolf.players[m.sender] = "villager"
+  werewolf.players[m.sender] = "قروي👨🏻‍🌾"
   werewolf.villagers.push(m.sender)
   global.db.data.werewolf = werewolf
-  conn.reply(m.sender, "You have joined the game as a villager", m)
+  conn.reply(m.sender, "لقد انضممت الى اللعبة كقروي👨🏻‍🌾", m)
 }
 handler.help = ['wwjoin']
-handler.tags = ['game']
-handler.command = /^wwjoin$/i
+handler.tags = ['العاب']
+handler.command = /^مستذئب_انضم$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
