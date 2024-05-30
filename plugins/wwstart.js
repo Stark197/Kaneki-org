8e86b81470b7cc1bc03d6c1b3d64890364beef03
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-30 21:51:06 +0100

    Update مستذئبين_ابدأ.js

--- plugins/wwstart.js
+++ plugins/wwstart.js
@@ -1,29 +1,29 @@
 let handler = async (m, { conn }) => {
   let werewolf = global.db.data.werewolf || {}
   if (typeof werewolf.status == "undefined" || werewolf.status != "playing") {
-    conn.reply(m.chat, "Werewolf game is not in progress", m)
+    conn.reply(m.chat, "اللعبة ليست قيد التقدم", m)
     return
   }
   if (Object.keys(werewolf.players).length < 5) {
-    conn.reply(m.chat, "At least 5 players are required to start the game", m)
+    conn.reply(m.chat, "مطلوب ما لا يقل عن 5 لاعبين لبدء اللعبة", m)
     return
   }
   let players = Object.keys(werewolf.players)
   let wolves = []
   while (wolves.length < Math.ceil(players.length / 3)) {
     let wolf = players[Math.floor(Math.random() * players.length)]
-    if (werewolf.players[wolf] == "villager") {
-      werewolf.players[wolf] = "wolf"
+    if (werewolf.players[wolf] == "قروي") {
+      werewolf.players[wolf] = "مستذئب"
       werewolf.wolves.push(wolf)
       wolves.push(wolf)
     }
   }
   global.db.data.werewolf = werewolf
-  conn.reply(m.chat, "Werewolf game started. There are " + players.length + " players, including " + werewolf.wolves.length + " wolves. Please send '.wwvote' to vote for a player", m)
+  conn.reply(m.chat, "اللعبة قد بدأت. هناك " + players.length + " لاعبين, يشمل " + werewolf.wolves.length + " مستذئبين. المرجو ارسال '.اختيار' لاختيار احد اللاعبين", m)
 }
 handler.help = ['wwstart']
 handler.tags = ['game']
-handler.command = /^wwstart$/i
+handler.command = /^مستذئب_ابدأ$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
