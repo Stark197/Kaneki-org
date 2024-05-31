42a295afb0f6c3248b179faf2369ba294f83463a
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-05-31 01:26:23 +0100

    Update and rename downloader-tiktok.js to التحميلات_تيكتوك.js

--- plugins/التحميلات_تيكتوك.js
+++ plugins/التحميلات_تيكتوك.js
@@ -1,9 +1,9 @@
 import { tiktok } from '../lib/tiktok.js'
 
 let handler = async(m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) return m.reply(`Masukan URL!\n\nContoh:\n${usedPrefix + command} https://vt.tiktok.com/ZS8oHC5Ka/`)
-    if (!/^http(s)?:\/\/(www|v(t|m)).tiktok.com\/[-a-zA-Z0-9@:%._+~#=]/i.test(args[0])) return m.reply('Invalid urls')
-    await m.reply('_In Progress Please Wait..._')
+    if (!args[0]) return m.reply(`لا تنسى الرابط!\n\nمثال:\n${usedPrefix + command} https://vt.tiktok.com/ZS8oHC5Ka/`)
+    if (!/^http(s)?:\/\/(www|v(t|m)).tiktok.com\/[-a-zA-Z0-9@:%._+~#=]/i.test(args[0])) return m.reply('رابط غير مدعوم')
+    await m.reply('_جاري التحميل المرجو الإنتظار..._')
     let { nickname, duration, description, play, music } = await tiktok(args[0])
     let caption = `
 Nickname : ${nickname}
@@ -15,7 +15,7 @@ ${description}
     await conn.sendFile(m.chat, music, false, false, video, false, { mimetype: 'audio/mpeg' })
  }
 handler.help = ['tiktok']
-handler.tags = ['downloader']
-handler.command = /^(tiktok|tiktok(mp3|mp4|video|audio)|tt|tt(mp3|mp4|video|audio))$/i
+handler.tags = ['التحميلات']
+handler.command = ['تيك']
 handler.limit = true
 export default handler
